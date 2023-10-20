cd23dfac6e1685fd5c84aca0e12c5cf1f4087559
maryam <seraji.maryam1982@gmail.com>
2023-10-21 00:10:37 +0330

    remove added in TODO exercise

--- todo.js
+++ todo.js
@@ -3,6 +3,7 @@ const title_input = document.querySelector("#title");
 const list = document.querySelector(".list");
 
 let todo_list = [];
+
 //work with DOM
 function renderItem(todo_item){
     //create element div with class item
@@ -18,21 +19,56 @@ function renderItem(todo_item){
     //add spam to item class
     const span = document.createElement("span");
     span.textContent = todo_item.title;
+
+     const deleteBtn = document.createElement("button");
+     deleteBtn.classList.add("delete-btn");;
+     deleteBtn.textContent = "delete" ;
   
     item.appendChild(checkbox);
     item.appendChild(span);
+    item.appendChild(deleteBtn);
     
     list.appendChild(item);
     checkbox.addEventListener("click",() => {
         toggleStatus(todo_item.title);
-        console.log("list", todo_list)
+        // console.log("list", todo_list)
        
     })
 
+    deleteBtn.addEventListener("click", () => {
+        console.log("salam");
+        remove(todo_item.title);
+        
+    })
+
+
 }
+
+function renderList(){
+
+    for(let i=0; i < todo_list.length; i++){
+        const item= todo_list[i];
+        renderItem(item);
+       }
+}
+
 function clearInput(){
     title_input.value = "";  
 }
+
+//work with Storage
+function syncStorage(){   
+    const next_list = JSON.stringify(todo_list);
+    console.log(todo_list, next_list)
+    localStorage.setItem("my_list", next_list);
+}
+
+function loadFromStorage() {
+  const listFromStorage= JSON.parse(localStorage.getItem("my_list")) || []
+  todo_list = listFromStorage;
+  } 
+
+   
 // functionality
 function toggleStatus(title){
     for (let i = 0; i < todo_list.length; i++ ){
@@ -58,43 +94,35 @@ function addItem(item){
     syncStorage();
 }
 
-function renderList(){
 
-    for(let i=0; i < todo_list.length; i++){
-        const item= todo_list[i];
-        renderItem(item);
-       }
-}
+function remove(val){         
+       for(let i = 0; i < todo_list.length; i++){
+           if (val === todo_list[i].title){
+            todo_list.splice(i,1);    
+          }
 
-//work with Storage
-function syncStorage(){   
-    const next_list = JSON.stringify(todo_list);
-    console.log(todo_list, next_list)
-    localStorage.setItem("my_list", next_list);
-}
+      }
+      console.log(todo_list)
+       syncStorage();
+       renderList()
+ }
+//Run your App
 
-
-function loadFromStorage() {
-  const listFromStorage= JSON.parse(localStorage.getItem("my_list")) || []
-  todo_list = listFromStorage;
-  } 
-
-   
 function onAddItem(){
-    const val = title_input.value;
-       //  console.log("clicked",value);
+    const val = title_input.value;       
        if(val=== ""){
            alert("You should write a text");
        }else {
          const item = {
             title:val,
-            status: true,
+            status: false,
          }      
          addItem(item);
          renderItem(item);
          clearInput();
        }
 }
+
 function events(){
     save_button.addEventListener("click", onAddItem);
 }
@@ -106,4 +134,3 @@ function init(){
 }
 
 init();
-
