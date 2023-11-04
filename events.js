bf69b526c6b61d4c9b8e45f99cfe1903413c2a79
mary <seraji.maryam1982@gmail.com>
2023-11-04 14:47:34 +0330

    function events added

---
+++ events.js
@@ -0,0 +1,93 @@
+import { syncStorage, loadFromStorage } from "./storage.js";
+import { renderList, renderItem, clearInput, title_input } from "./dom.js";
+import { addItem } from "./functionality.js";
+import { reset, todo_list } from "./store.js";
+
+const search_input = document.querySelector("#search");
+//Run your App
+
+export function onAddItem() {
+    const val = title_input.value;
+    if (val === "") {
+      alert("You should write a text");
+    } else {
+      const item = {
+        title: val,
+        status: false,
+      };
+      addItem(item);
+      syncStorage();
+      renderItem(item);
+      clearInput();
+    }
+  }
+  
+export function onDeleteAll() {
+    const new_items = todo_list.filter((item) => {
+      if (item.status === true) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    reset(new_items);
+    syncStorage();
+    renderList();
+  }
+export function onFilterState(e) {
+    let state = e.target.value;
+    let new_todo = todo_list;
+  
+    switch (state) {
+      case "done":
+        loadFromStorage();
+        new_todo = todo_list.filter((item) => {
+          if (item.status === true) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+        console.log(new_todo);
+        reset(new_todo);
+        break;
+      case "todo":
+        loadFromStorage();
+        new_todo = todo_list.filter((item) => {
+          if (item.status === true) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+  
+        console.log(new_todo);
+        reset(new_todo);
+        break;
+  
+      case "all":
+        loadFromStorage();
+        console.log(todo_list);
+        break;
+    }
+    renderList();
+  }
+  
+export function onSearchItem(event) {
+    event.preventDefault();
+    // console.log("bbbb");
+    loadFromStorage();
+    const new_items = todo_list.filter((item) => {
+        // console.log(item.title)
+        if (item.title.includes(search_input.value)){
+            //console.log(item.title,"yaftam")
+          return true;
+
+        } else {
+          return false;
+        }
+      });
+      
+      reset(new_items);
+      renderList();
+  }
--- todo.js
+++ todo.js
@@ -1,86 +1,14 @@
 import { syncStorage, loadFromStorage } from "./storage.js";
-import {todo_list } from "./store.js";
-import { renderItem, renderList, clearInput, title_input } from "./dom.js";
-import { addItem } from "./functionality.js";
+import { renderItem, renderList } from "./dom.js";
+import { onAddItem, onDeleteAll, onFilterState, onSearchItem } from "./events.js";
 
 const save_button = document.querySelector("#save-btn");
 const delete_button = document.querySelector("#delete");
 
 const item_filter = document.querySelector(".item-filter");
-const search_input = document.querySelector("#search");
 const search_button = document.querySelector("#search-btn");
 const todo_form = document.querySelector("#todo-form");
 
-
-
-//Run your App
-
-function onAddItem() {
-  const val = title_input.value;
-  if (val === "") {
-    alert("You should write a text");
-  } else {
-    const item = {
-      title: val,
-      status: false,
-    };
-    addItem(item);
-    syncStorage();
-    renderItem(item);
-    clearInput();
-  }
-}
-
-function onDeleteAll() {
-  const new_items = todo_list.filter((item) => {
-    if (item.status === true) {
-      return false;
-    } else {
-      return true;
-    }
-  });
-  todo_list = new_items;
-  syncStorage();
-  renderList();
-}
-function onFilterState(e) {
-  let state = e.target.value;
-  let new_todo = todo_list;
-
-  switch (state) {
-    case "done":
-      loadFromStorage();
-      new_todo = todo_list.filter((item) => {
-        if (item.status === true) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-      console.log(new_todo);
-      todo_list = new_todo;
-      break;
-    case "todo":
-      loadFromStorage();
-      new_todo = todo_list.filter((item) => {
-        if (item.status === true) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-
-      console.log(new_todo);
-      todo_list = new_todo;
-      break;
-
-    case "all":
-      loadFromStorage();
-      console.log(todo_list);
-      break;
-  }
-  renderList();
-}
 
 // function onSearchItem(event) {
 //   event.preventDefault();
@@ -91,15 +19,11 @@ function events() {
   //save_button.addEventListener("click", onAddItem);
   delete_button.addEventListener("click", onDeleteAll);
   item_filter.addEventListener("change",onFilterState);
-  // search_button.addEventListener("submit", onSearchItem);
+  search_button.addEventListener("click", onSearchItem);
   todo_form.addEventListener("submit", (ev) => {
     ev.preventDefault();
     onAddItem();
   });
-}
-function onSearchItem(event) {
-  event.preventDefault();
-  console.log(event , "mary event");
 }
 
 function init() {
