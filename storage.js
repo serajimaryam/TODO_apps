67e1f6278ee67bf80cf3bf0983dc3be832cd142e
mary <seraji.maryam1982@gmail.com>
2023-11-06 15:28:55 +0330

    draft modified

--- storage.js
+++ storage.js
@@ -1,20 +1,24 @@
 //work with Storage
 import {todo_list, reset} from "./store.js";
+export const storage_key = "todos"
+export const draft_char = "draft";
 
 export function syncStorage() {
     const next_list = JSON.stringify(todo_list);
     //console.log(todo_list, next_list);
-    localStorage.setItem("my_list", next_list);
+    localStorage.setItem(storage_key, next_list);
   }
   
 export function loadFromStorage() {
-    const listFromStorage = JSON.parse(localStorage.getItem("my_list")) || [];
+    const listFromStorage = JSON.parse(localStorage.getItem(storage_key)) || [];
     
     reset(listFromStorage);
   }
 
 export function updateDraft(char) {
-      const old_string = localStorage.getItem(draft_char) || "";
+      const old_string = localStorage.getItem(draft_char) || [];
+     // if char !="BAckspace" and 
       const new_string = old_string + char;
-      localStorage.setItem(draft_char, char);
+
+      localStorage.setItem(draft_char, new_string);
 }
